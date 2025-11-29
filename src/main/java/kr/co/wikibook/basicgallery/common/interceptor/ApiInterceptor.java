d3c25da52e2f6c7e3fbf53cc40eed74d538bc7f2
bermute <dl4946@naver.com>
2025-11-29 09:44:32 +0900

    인터셉터 로그인 체크용

--- src/main/java/kr/co/wikibook/basicgallery/common/interceptor/ApiInterceptor.java
+++ src/main/java/kr/co/wikibook/basicgallery/common/interceptor/ApiInterceptor.java
@@ -1,4 +1,25 @@
 package kr.co.wikibook.basicgallery.common.interceptor;
 
-public class ApiInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.wikibook.basicgallery.account.helper.AccountHelper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class ApiInterceptor implements HandlerInterceptor {
+
+    private final AccountHelper accountHelper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler){
+        if (accountHelper.getMemberId(req) == null) {
+            res.setStatus(401);
+            return false;
+        }
+
+        return true;
+    }
 }
