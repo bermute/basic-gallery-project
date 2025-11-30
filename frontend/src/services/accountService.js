cb72338984a2b9f1d9d9047535223f5c6a1856d5
bermute <dl4946@naver.com>
2025-11-30 11:45:08 +0900

    추가 개선 사항 로그인 인터셉트 백엔드 수정 및 axios 커스터마이징 핸들러 생성

--- frontend/src/services/accountService.js
+++ frontend/src/services/accountService.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import httpRequester from "@/libs/httpRequester";
 
 // 회원가입
 export const join = (args) => {// 회원 가입 처라 메서드
-    return axios.post("/v1/api/account/join", args).catch(e => e.response);
+    return httpRequester.post("/v1/api/account/join", args).catch(e => e.response);
 };
 
 // 로그인
 export const login = (args) => {// 로그인 처리하는 메서드
-    return axios.post("/v1/api/account/login", args).catch(e => e.response);
+    return httpRequester.post("/v1/api/account/login", args).catch(e => e.response);
 };
 
 // 로그인 여부 확인
 export const check = () => {// 로그인 여부 메서드
-    return axios.get("/v1/api/account/check").catch(e => e.response);
+    return httpRequester.get("/v1/api/account/check").catch(e => e.response);
 };
 
 // 로그아웃
 export const logout = () => { // 로그아웃 처리 메서드
-    return axios.post("/v1/api/account/logout").catch(e => e.response);
+    return httpRequester.post("/v1/api/account/logout").catch(e => e.response);
 };
